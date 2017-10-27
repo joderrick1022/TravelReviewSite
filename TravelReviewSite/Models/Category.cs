719f0c39f21ca316a1149c74a55807b288bbd26f
joderrick1022 <joderrick1022>
2017-10-27 13:12:09 -0400

    models setup

---
+++ TravelReviewSite/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewSite.Models
+{
+    public class Category
+    {
+    }
+}
---
+++ TravelReviewSite/Models/Review.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewSite.Models
+{
+    public class Review
+    {
+
+        [Key]
+        public  int ID { get; set; }
+        public string Name { get; set; }
+        public string Pros { get; set; }
+        public string Cons { get; set; }
+        public double Cost { get; set; }
+        public bool IsRecommended { get; set; }
+
+
+
+        [ForeignKey("Category")]
+        [Display(Name = "Category")]
+        public int CatagoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+
+    }
+}
